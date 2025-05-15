909505d1fd410c8a85b906a18abb738c36e08bab
Oliver Bing <ailurus842@gmail.com>
2025-05-15 10:42:13 +0200

    show indicator if wrong key is pressed

--- app/components/stop.tsx
+++ app/components/stop.tsx
@@ -8,6 +8,7 @@ export default function Stop({
   setStarted: (value: boolean) => void;
 }) {
   const [charNum, setCharNum] = React.useState(0);
+  const [isWrongKey, setIsWrongKey] = React.useState(false);
 
   const cleanedText = React.useMemo(() => {
     return text.replace(/\s+/g, " ").trim();
@@ -15,15 +16,16 @@ export default function Stop({
 
   const handleKeyDown = React.useCallback(
     (key: string) => {
-      if (key === "Backspace") setCharNum((prev) => prev - 1);
+      if (key === "Backspace") return setCharNum((prev) => prev - 1);
       const nextKey = cleanedText[charNum];
 
       if (key === nextKey) {
         setCharNum((prev) => prev + 1);
-      } else {
+      } else if (key !== "Shift" && key !== "AltGraph" && key !== "Control") {
+        setIsWrongKey(true);
+        setTimeout(() => setIsWrongKey(false), 250);
         console.log("wrong key", key, "next key was", nextKey);
       }
-      console.log(charNum);
     },
     [cleanedText, charNum]
   );
@@ -50,11 +52,15 @@ export default function Stop({
       </div>
 
       <div className="px-16 py-4 h-full w-full rounded-sm text-xl font-mono bg-purpleDark text-white">
-        <span className="text-greenMedium">
-          {cleanedText.slice(0, charNum)}
+        <span className="text-gray-500">{cleanedText.slice(0, charNum)}</span>
+        <span
+          className={`mx-[-5.5px] ${
+            isWrongKey ? "text-red-500" : "animate-pulse text-white"
+          }`}
+        >
+          |
         </span>
-        <span className="animate-pulse text-white mx-[-5.5px]">|</span>
-        <span className="text-gray-400">{cleanedText.slice(charNum)}</span>
+        <span className="text-gray-200">{cleanedText.slice(charNum)}</span>
       </div>
     </>
   );
