6057e68a362677883f416ebe69d72f274d752bc6
Olemio <ailurus842@gmail.com>
2025-08-06 15:38:03 +0200

    add load more button to leaderboard

--- app/routes/leaderboard.tsx
+++ app/routes/leaderboard.tsx
@@ -19,6 +19,7 @@ export default function Results() {
   const [modalData, setModalData] = React.useState<(typeof results)[0] | null>(
     null
   );
+  const [loadNum, setLoadNum] = React.useState(5);
 
   const handleGetResults = async () => {
     const dbData = await getAllUsersDB();
@@ -36,7 +37,7 @@ export default function Results() {
   return (
     <div className="flex flex-col items-center my-8 mx-4 text-purpleLight">
       <div className="flex flex-col items-center text-xl mb-8">
-        Top 5 Leaderboard{" "}
+        Leaderboard
         <p className="text-gray-400 font-mono text-sm">
           Score = WPM + Time (Max 15 sec)
         </p>
@@ -46,47 +47,55 @@ export default function Results() {
         {results.length === 0 ? (
           <p>Nothing here yet...</p>
         ) : (
-          results
-            .sort(
-              // Jeg har ikke brukt sort så mye før, denne er generert fra chat gpt. Med litt tweaking fra meg i ettertid
-              (a, b) =>
-                b.wpm +
-                (b.time > 15 ? 15 : b.time) -
-                (a.wpm + (a.time > 15 ? 15 : a.time))
-            )
-            .slice(0, 6)
-            .map((result, i) => (
-              <button
-                key={result.id}
-                onClick={() => {
-                  setModalData(result);
-                  setShowModal(true);
-                }}
-                className="flex gap-8 justify-between bg-purpleDark px-6 py-4 rounded w-full max-w-4xl"
-              >
-                <div className="flex gap-4">
-                  <p>{i}.</p>
-                  <p>User</p>
-                  <p className="font-mono text-gray-400">
-                    {result?.email ?? "Test"}
-                  </p>
-                </div>
-                <div className="flex gap-4">
-                  <p>WPM</p>
-                  <p className="font-mono text-gray-400">{result.wpm}</p>
-                  <p>Time</p>
-                  <p className="font-mono text-gray-400">
-                    {formatTime(result.time)}
-                  </p>
-                  <p>Score</p>
-                  <p className="font-mono text-gray-400">
-                    {(
-                      result.wpm + (result.time > 15 ? 15 : result.time)
-                    ).toFixed(2)}
-                  </p>
-                </div>
-              </button>
-            ))
+          <>
+            {results
+              .sort(
+                // Jeg har ikke brukt sort så mye før, denne er generert fra chat gpt. Med litt tweaking fra meg i ettertid
+                (a, b) =>
+                  b.wpm +
+                  (b.time > 15 ? 15 : b.time) -
+                  (a.wpm + (a.time > 15 ? 15 : a.time))
+              )
+              .slice(0, loadNum)
+              .map((result, i) => (
+                <button
+                  key={result.id}
+                  onClick={() => {
+                    setModalData(result);
+                    setShowModal(true);
+                  }}
+                  className="flex gap-8 justify-between bg-purpleDark px-6 py-4 rounded w-full max-w-4xl"
+                >
+                  <div className="flex gap-4">
+                    <p>{i + 1}.</p>
+                    <p>User</p>
+                    <p className="font-mono text-gray-400">
+                      {result?.email ?? "Test"}
+                    </p>
+                  </div>
+                  <div className="flex gap-4">
+                    <p>WPM</p>
+                    <p className="font-mono text-gray-400">{result.wpm}</p>
+                    <p>Time</p>
+                    <p className="font-mono text-gray-400">
+                      {formatTime(result.time)}
+                    </p>
+                    <p>Score</p>
+                    <p className="font-mono text-gray-400">
+                      {(
+                        result.wpm + (result.time > 15 ? 15 : result.time)
+                      ).toFixed(2)}
+                    </p>
+                  </div>
+                </button>
+              ))}
+            <button
+              onClick={() => setLoadNum((prev) => prev + 10)}
+              className="flex gap-8 justify-between bg-purpleDark px-6 py-4 rounded w-full max-w-4xl"
+            >
+              Load more...
+            </button>
+          </>
         )}
       </div>
       {modalData && (
